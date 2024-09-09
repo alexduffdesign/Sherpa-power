21cfd74ae4abcbda1da5355a4c87eccdf00c4417
alexduffdesign <alexduffblog@gmail.com>
2024-09-09 18:29:07 +0100

    Add the assets

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,4 @@ import shopify from "vite-plugin-shopify";
 
 export default {
   plugins: [shopify()],
-  optimizeDeps: {
-    exclude: ["assets/theme.css"], // Exclude your main theme CSS
-  },
 };
