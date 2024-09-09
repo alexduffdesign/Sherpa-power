bd9ee209e421f2fd681493dda4a961fa1314cc68
alexduffdesign <alexduffblog@gmail.com>
2024-09-09 18:10:35 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,4 +2,7 @@ import shopify from "vite-plugin-shopify";
 
 export default {
   plugins: [shopify()],
+  optimizeDeps: {
+    exclude: ["assets/theme.css"], // Exclude your main theme CSS
+  },
 };
