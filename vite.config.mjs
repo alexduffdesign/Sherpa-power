c18a2b781eda5ba1596d01189c5a930addb404df
alexduffdesign <alexduffblog@gmail.com>
2024-09-09 18:43:14 +0100

    vites

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import shopify from "vite-plugin-shopify";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [shopify()],
   build: {
     emptyOutDir: false, // Prevent Vite from clearing the assets directory
-    rollupOptions: {
-      input: {
-        "vite-vendor": resolve(__dirname, "src/js/vendor.js"),
-        "vite-theme": resolve(__dirname, "src/js/theme.js"),
-        "vite-sections": resolve(__dirname, "src/js/sections.js"),
-        "vite-chatbot-core": resolve(__dirname, "src/js/chatbot-core-file.js"),
-        "vite-chatbot-main": resolve(__dirname, "src/js/chatbot-main.js"),
-      },
-      output: {
-        dir: "assets",
-        entryFileNames: "vite-[name].js",
-        chunkFileNames: "vite-[name]-[hash].js",
-        assetFileNames: "vite-[name]-[hash][extname]",
-      },
-    },
   },
   optimizeDeps: {
     exclude: ["assets/theme.css"], // Exclude the main theme CSS from Vite processing
